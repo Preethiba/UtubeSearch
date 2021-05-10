29276a47d7bfc3aa107bda88f28805aa912a7cf4
Preethiba <ibpreethiba99@gmail.com>
2021-05-10 10:09:54 +0530

    3 files modified

---
+++ src/components/VideoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VideoList = props => {
+  console.log(props);
+
+  return (
+    <ul className="col-md-4 list-group">
+      <li />
+    </ul>
+  );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 
 const API_KEY = 'AIzaSyAFRoRZZYCT0cjDNru8fAuw-QTuSMGdUz0';
 
-YTSearch({ key: API_KEY, term: 'Vijay' }, function(data) {
-  console.log(data);
-});
-
 //Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'Vijay' }, videos => {
+      this.setState({ videos }); //this.setState({ videos: videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 //Take this component's generated HTML and put it on the page (DOM)
 //App - component class
