7758abac220416188095240c61274d13cc325b53
Preethiba <ibpreethiba99@gmail.com>
2021-05-12 11:41:42 +0530

    3 files modified

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
 import VideoDetail from './components/VideoDetail';
 
-const API_KEY = 'AIzaSyDawqb7xiYpedtKrp1DTeW2H5EgBYLp1ks';
+const API_KEY = 'AIzaSyA86_SPWy-WA-G3YoJ6fdb6_5g6o_h-3Ac';
 
 //Create a new component. This component should produce some HTML
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { videos: [], selectedVideo: null };
-    YTSearch({ key: API_KEY, term: 'Vijay' }, videos => {
+    this.videoSearch('Dhanush');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState({ videos }); //this.setState({ videos: videos });
       this.setState({ selectedVideo: videos[0] });
     });
   }
+
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300); // called once in 300ms
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
